5bf444b8d5b31f027baff1720230e5013683f83f
Ravi-Konkepudi <ravikalyanc@2xprime.com>
2014-08-07 20:40:17 +0530

    initial version

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[] 
+        { 
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+
+        // GET api/products
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        // GET api/products/5
+        public Product Get(int id)
+        {
+            var product = products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                throw new Exception("Product Not Found");
+            }
+            return product;
+        }
+
+        // POST api/products
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
